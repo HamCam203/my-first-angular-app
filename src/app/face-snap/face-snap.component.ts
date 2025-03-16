82c70b10666e2d5ff5fd5dc3c312c14e10de44c0
HamCam203 <camarahamoudia@gmail.com>
2025-03-16 15:37:48 +0100

    feat: add French locale support and update FaceSnap creation date format

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { NgClass, NgStyle, TitleCasePipe } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [NgStyle, NgClass,TitleCasePipe],
+  imports: [NgStyle, NgClass,TitleCasePipe,DatePipe],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
